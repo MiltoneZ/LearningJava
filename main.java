7cf24a5863c3014eca07e2f8203fea8f99be4a17
MiltoneZ <milton.uzieldm@gmail.com>
2024-04-07 20:53:04 -0600

    Declaracion de variables e imprimir variables y texto.

--- main.java
+++ main.java
@@ -1,5 +1,14 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("Alura");
+        System.out.println("Bienvenido a Screen Match");
+
+        // Asi se declaran las variables, tienen que llevar el tipo de variable que va a ser.
+        // No puedes declarar una variable sin decirle que tipo de variable es.
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnElPlan = true;
+        double notaDeLaPelicula = 8.2;
+
+        double media = (8.2 + 6.0 + 9.0) /3;
+        System.out.println("La media de las notas es: " + media);
     }
 }
