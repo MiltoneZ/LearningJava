421c109a173b1c5201d02cc9a2e929c14162ff35
MiltoneZ <milton.uzieldm@gmail.com>
2024-04-09 18:51:45 -0600

    Conversion de tipos de datos a otros.

--- Main.java
+++ Main.java
@@ -9,7 +9,19 @@ public class Main {
         boolean incluidoEnElPlan = true;
         double notaDeLaPelicula = 8.2;
 
-        double media = (8.2 + 6.0 + 9.0) /3;
+        double media = (8.2 + 9.2 + 9.0) /3;
         System.out.println("La media de las notas es: " + media);
+
+        String sinopsis = """
+                Matrix es una paradoja.
+                La mejor pelicula del fin del milenio.
+                Fue lanzada en:
+                """ + fechaDeLanzamiento;
+        System.out.println(sinopsis);
+
+        // Se pueden convertir diferentes tipos de datos en otro, pero para ello.
+        // Se necesita revisar las tablas de conversiones de tipos de datos.
+        int clasificacion = (int) (media / 2);
+        System.out.println("Estrellas: " + clasificacion);
     }
 }
